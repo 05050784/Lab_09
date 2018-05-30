de61034861256db4230dc5177172d360bb2eecaa
05050784 <37134163+05050784@users.noreply.github.com>
2018-05-30 15:26:18 +0800

    Add files via upload

---
+++ check2.ino
@@ -0,0 +1,21 @@
+#include<IRremote.h>
+  
+int buttonSate =0;
+IRsend irsend ;
+void setup() 
+{
+       // put your setup code here, to run once:
+pinMode(3,OUTPUT);
+}
+  
+void loop()
+{
+buttonSate = digitalRead(6);
+         if(buttonSate){
+                 digitalWrite(3,1);
+irsend.sendNEC(0x4FB48B7,32);
+}
+delay(100);
+ digitalWrite(3,0);
+}
+
